342e0d788ed57133c1cb4434e3f92e97f7dd1bd9
Leyla <lejla.eyvazova@gmail.com>
2023-11-07 16:38:02 +0700

    git cache cleared

--- src/components/burger-tab/burger-tab.jsx
+++ src/components/burger-tab/burger-tab.jsx
@@ -3,16 +3,16 @@ import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './burger-tab.module.css';
 import PropTypes from 'prop-types';
 
-function BurgerTab({ current, setCurrent }) {
+function BurgerTab({ current }) {
   return (
     <div className={styles.burger_tab}>
-      <Tab value="one" active={current === 'one'} onClick={setCurrent}>
+      <Tab value="one" active={current === 'one'}>
         Булки
       </Tab>
-      <Tab value="two" active={current === 'two'} onClick={setCurrent}>
+      <Tab value="two" active={current === 'two'}>
         Соусы
       </Tab>
-      <Tab value="three" active={current === 'three'} onClick={setCurrent}>
+      <Tab value="three" active={current === 'three'}>
         Начинки
       </Tab>
     </div>
