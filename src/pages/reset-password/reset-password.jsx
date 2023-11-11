515546a076e7596eb8f7cace9c000bc4c43ca10d
Leyla <lejla.eyvazova@gmail.com>
2023-11-11 19:29:18 +0700

    write the same fetch for reset-password

--- src/pages/reset-password/reset-password.jsx
+++ src/pages/reset-password/reset-password.jsx
@@ -11,14 +11,24 @@ import { Link } from 'react-router-dom';
 function ResetPassword() {
 
     const [password, setPassword] = useState('password')
+    const [token, setToken] = useState('token');
     const onChange = e => {
         setPassword(e.target.password)
     }
 
-    const handleResetPassword = () => {
-        resetPassword(password);
-    }
+    const handleResetPassword = async () => {
+        const apiUrl = 'https://norma.nomoreparties.space/api';
+        const endPoint = './password-reset/reset';
+        const options = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ password, token }),
+        };
 
+        const res = await fetch(`${apiUrl}/${endPoint}`, options);
+
+        console.log('Response:', res);
+    };
     return (
         <section className={styles.reset_password}>
             <h2 className={`${styles.reset_password_title} text text_type_main-medium`}>Восстановление пароля</h2>
@@ -37,6 +47,8 @@ function ResetPassword() {
                 type={'text'}
                 error={false}
                 placeholder={'Введите код из письма'}
+                value={token}
+                onChange={(e) => setToken(e.target.value)}
                 extraClass="mb-2" />
 
 
--- src/utils/api.js
+++ src/utils/api.js
@@ -53,7 +53,7 @@ export async function forgotPassword(email) {
     });
 }
 
-// //создание нового пользователя
+//эндпоинт для регистрации пользователя
 export const userRegister = createAsyncThunk(
     'auth/register',
     async (email, password, name) => {
@@ -77,138 +77,156 @@ export const userRegister = createAsyncThunk(
 );
 
 
+//обновление токена, когда тот устаревает через 20 мин
+const refreshToken = () => {
+    return fetch(`${apiUrl}/auth/token`, {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json;charset=utf-8"
+        },
+        body: JSON.stringify({
+            token: localStorage.getItem("refreshToken")
+        })
+    }).then(checkResponse);
+};
+
+//ф-ция, которая делает запрос к серверу и проверяет токен: запрос с обновлением
+//в параметры принимает все те данные, которые уходят в обычный fetch
+//ф-ция должна подменить fetch для выполнения запроса с проверкой токена
+const fetchWithRefresh = async (url, options) => {
+    try {
+        const res = await fetch(url, options);
+        return await checkResponse(res);
+    } catch (err) {
+        if (err.message === "jwt expired") {
+            const refreshData = await refreshToken();
+            if (!refreshData.success) {
+                return Promise.reject(refreshData);
+            }
+            localStorage.setItem("accessToken", refreshData.accessToken);
+            localStorage.setItem("refreshToken", refreshData.refreshToken);
+            options.headers.authorization = refreshData.accessToken;
+            const res = await fetch(url, options);
+            return await checkResponse(res);
+        } else {
+            return Promise.reject(err);
+        }
+    }
+};
+
+//проверяем пользователя: для этого делаем запрос данных к серверу;
+//если данные успешные, то п-ль авторизован
+//если нет – очищаем данные пользователя
+export const getUser = () => {
+    return (dispatch) => {
+        return fetchWithRefresh(`${apiUrl}/auth/user`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                authorization: localStorage.getItem("accessToken")
+            }
+        }).then((res) => {
+            if (res.success) {
+                dispatch(setUser(res.user));
+            } else {
+                return Promise.reject("Ошибка данных с сервера");
+            }
+        });
+    };
+};
+
+//эндпоинт для авторизации
+//получаем email и password, делаем запрос на сервер
+//если запрос успешный, то получаем токены и диспатчим нового пользователя,
+// сохраняем его в хранилище
+export const login = (email, password) => {
+    return (dispatch) => {
+        return fetch(`${apiUrl}/auth/login`, {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json;charset=utf-8"
+            },
+            body: JSON.stringify({
+                email: email,
+                password: password
+            })
+        })
+            .then(checkResponse)
+            .then((res) => {
+                if (res.success) {
+                    localStorage.setItem("accessToken", res.accessToken);
+                    localStorage.setItem("refreshToken", res.refreshToken);
+                    dispatch(setUser(res.user));
+                } else {
+                    return Promise.reject("Ошибка данных с сервера");
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+            //завершение авторизации, которая проверяется в компоненте protected-route: 
+            //ждем, когда завершится процесс авторизации, 
+            //чтобы отобразить какой-то компонент
+            .finally(() => {
+                dispatch(setAuthChecked(true));
+            });
+    };
+};
+//эндпоинт для выхода из системы
+export const logout = (email, password) => {
+    return (dispatch) => {
+        return fetch(`${apiUrl}/auth/logout`, {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json;charset=utf-8"
+            },
+            body: JSON.stringify({
+                email: email,
+                password: password
+            })
+        })
+            .then(checkResponse)
+            .then((res) => {
+                if (res.success) {
+                    localStorage.setItem("accessToken", res.accessToken);
+                    localStorage.setItem("refreshToken", res.refreshToken);
+                    dispatch(setUser(res.user));
+                } else {
+                    return Promise.reject("Ошибка данных с сервера");
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+            //завершение авторизации, которая проверяется в компоненте protected-route: 
+            //ждем, когда завершится процесс авторизации, 
+            //чтобы отобразить какой-то компонент
+            .finally(() => {
+                dispatch(setAuthChecked(true));
+            });
+    };
+};
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// //обновление токена, когда тот устаревает через 20 мин
-// const refreshToken = () => {
-//     return fetch(`${apiUrl}/auth/token`, {
-//         method: "POST",
-//         headers: {
-//             Accept: "application/json",
-//             "Content-Type": "application/json;charset=utf-8"
-//         },
-//         body: JSON.stringify({
-//             token: localStorage.getItem("refreshToken")
-//         })
-//     }).then(checkResponse);
-// };
-
-// //ф-ция, которая делает запрос к серверу и проверяет токен: запрос с обновлением
-// //в параметры принимает все те данные, которые уходят в обычный fetch
-// //ф-ция должна подменить fetch для выполнения запроса с проверкой токена
-// const fetchWithRefresh = async (url, options) => {
-//     try {
-//         const res = await fetch(url, options);
-//         return await checkResponse(res);
-//     } catch (err) {
-//         if (err.message === "jwt expired") {
-//             const refreshData = await refreshToken();
-//             if (!refreshData.success) {
-//                 return Promise.reject(refreshData);
-//             }
-//             localStorage.setItem("accessToken", refreshData.accessToken);
-//             localStorage.setItem("refreshToken", refreshData.refreshToken);
-//             options.headers.authorization = refreshData.accessToken;
-//             const res = await fetch(url, options);
-//             return await checkResponse(res);
-//         } else {
-//             return Promise.reject(err);
-//         }
-//     }
-// };
-
-// //проверяем пользователя: для этого делаем запрос данных к серверу;
-// //если данные успешные, то п-ль авторизован
-// //если нет – очищаем данные пользователя
-// export const getUser = () => {
-//     return (dispatch) => {
-//         return fetchWithRefresh(`${apiUrl}/auth/user`, {
-//             method: "GET",
-//             headers: {
-//                 "Content-Type": "application/json",
-//                 authorization: localStorage.getItem("accessToken")
-//             }
-//         }).then((res) => {
-//             if (res.success) {
-//                 dispatch(setUser(res.user));
-//             } else {
-//                 return Promise.reject("Ошибка данных с сервера");
-//             }
-//         });
-//     };
-// };
-
-// //получаем email и password, делаем запрос на сервер
-// //если запрос успешный, то получаем токены и диспатчим нового пользователя,
-// // сохраняем его в хранилище
-// export const login = (email, password) => {
-//     return (dispatch) => {
-//         return fetch(`${apiUrl}/auth/login`, {
-//             method: "POST",
-//             headers: {
-//                 Accept: "application/json",
-//                 "Content-Type": "application/json;charset=utf-8"
-//             },
-//             body: JSON.stringify({
-//                 email: email,
-//                 password: password
-//             })
-//         })
-//             .then(checkResponse)
-//             .then((res) => {
-//                 if (res.success) {
-//                     localStorage.setItem("accessToken", res.accessToken);
-//                     localStorage.setItem("refreshToken", res.refreshToken);
-//                     dispatch(setUser(res.user));
-//                 } else {
-//                     return Promise.reject("Ошибка данных с сервера");
-//                 }
-//             })
-//             .catch((err) => {
-//                 console.log(err);
-//             })
-//             //завершение авторизации, которая проверяется в компоненте protected-route: 
-//             //ждем, когда завершится процесс авторизации, 
-//             //чтобы отобразить какой-то компонент
-//             .finally(() => {
-//                 dispatch(setAuthChecked(true));
-//             });
-//     };
-// };
-
-// //если в localStorage сохранен accessToken, то тогда мы вызываем getUser
-// //в getUser делаем запрос – если он проходит, то запоминаем пользователя
-// export const checkUserAuth = () => {
-//     return (dispatch) => {
-//         if (localStorage.getItem('accessToken')) {
-//             dispatch(getUser())
-//                 .catch((error) => {
-//                     localStorage.removeItem('accessToken');
-//                     localStorage.removeItem('refreshToken');
-//                     dispatch(setUser(null));
-//                 })
-//                 .finally(() => dispatch(setAuthChecked(true)));
-//         } else {
-//             dispatch(setAuthChecked(true));
-//             dispatch(setUser(null));
-//         }
-//     };
-// };
+//если в localStorage сохранен accessToken, то тогда мы вызываем getUser
+//в getUser делаем запрос – если он проходит, то запоминаем пользователя
+export const checkUserAuth = () => {
+    return (dispatch) => {
+        if (localStorage.getItem('accessToken')) {
+            dispatch(getUser())
+                .catch((error) => {
+                    localStorage.removeItem('accessToken');
+                    localStorage.removeItem('refreshToken');
+                    dispatch(setUser(null));
+                })
+                .finally(() => dispatch(setAuthChecked(true)));
+        } else {
+            dispatch(setAuthChecked(true));
+            dispatch(setUser(null));
+        }
+    };
+};
 
