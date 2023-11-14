8a6122f660f1b60550d7cf01d6eefada7c7196f4
Leyla <lejla.eyvazova@gmail.com>
2023-11-14 15:31:41 +0700

    commit

--- src/pages/ingredient-page/ingredient-page.jsx
+++ src/pages/ingredient-page/ingredient-page.jsx
@@ -1,14 +1,25 @@
 import styles from './ingredient-page.module.css';
 import { selectIngredients } from '../../services/selectors/ingredientsSelector';
 
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
 import IngredientDetails from '../../components/ingredient-details/ingredient-details';
+import { getIngredientsData } from '../../services/reducers/ingredientsReducer';
 
 function IngredientPage() {
+
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(getIngredientsData());
+    }, [dispatch]);
+
     const { id } = useParams();
+    console.log("Ingredient ID:", id);
     const ingredients = useSelector(selectIngredients);
+    console.log("Ingredients:", ingredients);
     const ingredientsData = ingredients.find((ingredient) => ingredient._id === id);
 
     return (
