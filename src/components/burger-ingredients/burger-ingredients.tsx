6e45d78d8f4ed755a6d0d47997216d881f0f267d
Leyla <lejla.eyvazova@gmail.com>
2023-12-07 19:24:37 +0700

    commit

--- src/components/burger-ingredients/burger-ingredients.tsx
+++ src/components/burger-ingredients/burger-ingredients.tsx
@@ -19,128 +19,109 @@ import { Ingredient } from '../../types/ingredient-types';
 
 
 const BurgerIngredients: FC = () => {
+  const dispatch = useDispatch();
+  const [current, setCurrent] = useState('one');
+  const ingredients = useSelector(selectIngredients);
+  const modalType = useSelector(SelectModalType);
+  const ingredientDetailsIsLoading = useSelector(selectIngredientsIsLoading);
+  const ingredientDetailsIsError = useSelector(selectIngredientsError);
 
-    const dispatch = useDispatch();
+  const oneRef = useRef<HTMLHeadingElement>(null);
+  const twoRef = useRef<HTMLHeadingElement>(null);
+  const threeRef = useRef<HTMLHeadingElement>(null);
 
-    const [current, setCurrent] = useState('one');
-    const ingredients = useSelector(selectIngredients) as Array<Ingredient>;
+  interface IResult {
+    name: string;
+    coords: number | undefined;
+  }
 
-    const modalType = useSelector(SelectModalType);
-    const ingredientDetailsIsLoading = useSelector(selectIngredientsIsLoading);
-    const ingredientDetailsIsError = useSelector(selectIngredientsError);
-    // const ingredient = useSelector(selectCurrentIngredient) as Ingredient;
+  const handleScroll = () => {
+    const result: IResult[] = [
+      {
+        name: 'one',
+        coords: oneRef.current?.getBoundingClientRect().top,
+      },
+      {
+        name: 'two',
+        coords: twoRef.current?.getBoundingClientRect().top,
+      },
+      {
+        name: 'three',
+        coords: threeRef.current?.getBoundingClientRect().top,
+      },
+    ]
+      .filter((elem) => elem.coords && elem.coords > 0)
+      .sort((a, b) => Number(a.coords) - Number(b.coords));
 
-    const oneRef = useRef<HTMLHeadingElement>(null);
-    const twoRef = useRef<HTMLHeadingElement>(null);
-    const threeRef = useRef<HTMLHeadingElement>(null);
+    if (result.length) {
+      setCurrent(result[0].name);
+    }
+  };
 
-    interface IResult {
-        name: string;
-        coords: number | undefined;
-      }
-      
-      const handleScroll = () => {
-        const result: IResult[] = [
-          {
-            name: 'one',
-            coords: oneRef.current?.getBoundingClientRect().top,
-          },
-          {
-            name: 'two',
-            coords: twoRef.current?.getBoundingClientRect().top,
-          },
-          {
-            name: 'three',
-            coords: threeRef.current?.getBoundingClientRect().top,
-          },
-        ]
-        .filter((elem) =>elem.coords && elem.coords > 0)
-        .sort((a, b) => Number(a.coords) - Number(b.coords));
-      
-        if (result.length) {
-          setCurrent(result[0].name);
-        }
-      };
-    return (
-        <>
-            <section className={styles.burger_ingredients}>
-                <h2 className={`${styles.burger_title} text text_type_main-large`}>Соберите бургер</h2>
-                <BurgerTab current={current} setCurrent={setCurrent} />
-                <article className={styles.container} onScroll={handleScroll}>
-            
-            {ingredients && ingredients.length > 0 && (
-  <>
-    <h3 className={`${styles.ingredient_title} text text_type_main-medium pb-6`} ref={oneRef}>
-      Булки
-    </h3>
-    <div className={`${styles.ingredients} pl-4`}>
-      {console.log('Ingredients:', ingredients)}
-      {ingredients.map((ingredient: Ingredient) => {
-        if (ingredient.type === 'bun') {
-          return (
-            <IngredientItem
-              key={ingredient._id}
-              _id={ingredient._id}
-              ingredient={ingredient}
-            />
-          );
-        }
-      })}
-    </div>
+  console.log('Ingredients:', ingredients);
 
-    <h3 className={`${styles.ingredient_title} text text_type_main-medium pb-6`} ref={twoRef}>
-      Соусы
-    </h3>
+  return (
+    <>
+      <section className={styles.burger_ingredients}>
+        <h2 className={`${styles.burger_title} text text_type_main-large`}>Соберите бургер</h2>
+        <BurgerTab current={current} setCurrent={setCurrent} />
+        <div className={styles.container} onScroll={handleScroll}>
+          {ingredients && ingredients.length > 0 ? (
+            <>
+              <h3 className={`${styles.ingredient_title} text text_type_main-medium pb-6`} ref={oneRef}>
+                Булки
+              </h3>
+              <div className={`${styles.ingredients} pl-4`}>
+                {ingredients.map((ingredient: Ingredient) => {
+                  if (ingredient.type === 'bun') {
+                    return <IngredientItem key={ingredient._id} _id={ingredient._id} ingredient={ingredient} />;
+                  }
+                })}
+              </div>
 
-    <div className={`${styles.ingredients} pl-4`}>
-      {ingredients.map((ingredient: Ingredient) => {
-        if (ingredient.type === 'sauce') {
-          return (
-            <IngredientItem
-              key={ingredient._id}
-              _id={ingredient._id}
-              ingredient={ingredient}
-            />
-          );
-        }
-      })}
-    </div>
+              <h3 className={`${styles.ingredient_title} text text_type_main-medium pb-6`} ref={twoRef}>
+                Соусы
+              </h3>
 
-    <h3 className={`${styles.ingredient_title} text text_type_main-medium pb-6`} ref={threeRef}>
-      Начинки
-    </h3>
+              <div className={`${styles.ingredients} pl-4`}>
+                {ingredients.map((ingredient: Ingredient) => {
+                  if (ingredient.type === 'sauce') {
+                    return <IngredientItem key={ingredient._id} _id={ingredient._id} ingredient={ingredient} />;
+                  }
+                })}
+              </div>
 
-    <div className={`${styles.ingredients} pl-4`}>
-      {ingredients.map((ingredient: Ingredient) => {
-        if (ingredient.type === 'main') {
-          return (
-            <IngredientItem
-              key={ingredient._id}
-              _id={ingredient._id}
-              ingredient={ingredient}
-            />
-          );
-        }
-      })}
-    </div>
-  </>
-)}
-                </article>
-            </section>
+              <h3 className={`${styles.ingredient_title} text text_type_main-medium pb-6`} ref={threeRef}>
+                Начинки
+              </h3>
 
-            {modalType === 'ingredientDetails' && (
-                <Modal title={'Детали ингредиента'} onClose={() => dispatch(modalClose())}>
-                    {ingredientDetailsIsLoading ? (
-                        <span className="text text_type_main-medium mt-8 mb-8">Загрузка...</span>
-                    ) : ingredientDetailsIsError ? (
-                        <span className="text text_type_main-medium mt-8 mb-8">Ошибка</span>
-                    ) : (
-                        <IngredientDetails />
-                    )}
-                </Modal>
-            )}
-        </>
-    );
+              <div className={`${styles.ingredients} pl-4`}>
+                {ingredients.map((ingredient: Ingredient) => {
+                  if (ingredient.type === 'main') {
+                    return <IngredientItem key={ingredient._id} _id={ingredient._id} ingredient={ingredient} />;
+                  }
+                })}
+              </div>
+            </>
+          ) : (
+            <p>No ingredients available.</p>
+          )}
+        </div>
+      </section>
+
+      {modalType === 'ingredientDetails' && (
+        <Modal title={'Детали ингредиента'} onClose={() => dispatch(modalClose())}>
+          {ingredientDetailsIsLoading ? (
+            <span className="text text_type_main-medium mt-8 mb-8">Загрузка...</span>
+          ) : ingredientDetailsIsError ? (
+            <span className="text text_type_main-medium mt-8 mb-8">Ошибка</span>
+          ) : (
+            <IngredientDetails />
+          )}
+        </Modal>
+      )}
+    </>
+  );
 };
 
 export default BurgerIngredients;
