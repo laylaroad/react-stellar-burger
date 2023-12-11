bb161a8709a89c5e820a2bedf5205f5ae996b7ac
Leyla <lejla.eyvazova@gmail.com>
2023-12-11 22:50:10 +0700

    commit

--- src/pages/profile/orders-history/orders-history.tsx
+++ src/pages/profile/orders-history/orders-history.tsx
@@ -1,28 +1,61 @@
 import styles from './orders-history.module.css';
 import { FC, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import Order from '../../../components/order/order';
 import { wssUrl } from '../../../utils/api';
 import { useAppDispatch, useAppSelector } from '../../../hooks/react-redux';
-
 import { IOrder } from '../../../types/order-types';
+import { setCurrentOrder } from '../../../services/reducers/ordersFeedReducer';
+import { modalOpen } from '../../../services/reducers/modalReducer';
+import order from '../../../components/order/order';
+
+
+import { wsConnect } from '../../../services/reducers/wsActions';
+
 
 const OrdersHistory: FC = () => {
-    // const location = useLocation();
-    // const dispatch = useAppDispatch();
+    const location = useLocation();
+    const dispatch = useAppDispatch();
     // const accessToken = localStorage.getItem("accessToken")?.split("Bearer ")[1];
 
-    // const selectAllOrders = (store: any) => store.feedApi.allOrders;
+    const selectAllOrders = (store: any) => store.feedApi.allOrders;
+    const allOrders = useAppSelector(selectAllOrders);
 
-    // const allOrders = useAppSelector(selectAllOrders);
+    const modalOrderInfo = () => {
+        dispatch(setCurrentOrder(order))
+        dispatch(modalOpen('order-info'));
+    };
 
-    return (
-        // <div className={styles.orders}>
-        //     {allOrders.orders.map((order: IOrder) => (
-        //         <OrderList key={order._id} order={order} />
-        //     ))}
-        // </div>
-        (null)
-    )
+    useEffect(() => {
+        // console.log('Connecting to WebSocket...');
+        dispatch(wsConnect('wss://norma.nomoreparties.space/orders/all'));
+    })
+
+    if (allOrders) {
+        return (
+            <section className={styles.feed}>
+                <ul className={styles.orders}>
+                    {allOrders.orders.map((order: IOrder) => (
+                        <Link
+                            style={{ textDecoration: 'none' }}
+                            state={{ background: location }}
+                            to={`${order._id}`}
+                            onClick={(e: any) => {
+                                modalOrderInfo();
+                            }}
+                            key={order._id}
+                        >
+                            <li>
+                                <Order key={order._id} order={order} />
+                            </li>
+                        </Link>
+                    ))}
+                </ul>
+            </section>
+        )
+    } else {
+        return <>loading</>;
+    }
 }
 
 export default OrdersHistory;
